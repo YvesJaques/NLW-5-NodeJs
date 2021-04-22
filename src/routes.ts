d7e2b419ae5de1a98b6b23986eab0dd51fc315fe
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-04-22 10:29:53 -0300

    Criacao do SettingsController

--- src/routes.ts
+++ src/routes.ts
@@ -1,27 +1,12 @@
 import { Router } from "express";
 import { getCustomRepository } from "typeorm";
+import { SettingsController } from "./controllers/SettingsController";
 import { SettingsRepository } from "./repositories/SettingsRepository";
-
-
 
 const routes = Router();
 
+const settingsController = new SettingsController();
 
-routes.post("/settings", async (request, response) => {
-    const { chat, username } = request.body;
-
-    const settingsRepository = getCustomRepository(SettingsRepository);
-
-    const settings = settingsRepository.create({
-        chat,
-        username
-    })
-
-    settingsRepository.save(settings);
-
-    await settingsRepository.save(settings);
-
-    return response.json(settings);
-})
+routes.post("/settings", settingsController.create)
 
 export { routes }
