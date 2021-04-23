ffeb3d45ac8216606398bbff4c7847213b321a0e
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-04-23 08:14:23 -0300

    CreateUsers migration

--- src/database/migrations/1619176147857-CreateUsers.ts
+++ src/database/migrations/1619176147857-CreateUsers.ts
@@ -1,11 +1,11 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateSettings1619056892276 implements MigrationInterface {
+export class CreateUsers1619176147857 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: "settings",
+                name: "users",
                 columns: [
                     {
                         name: "id",
@@ -13,31 +13,21 @@ export class CreateSettings1619056892276 implements MigrationInterface {
                         isPrimary: true
                     },
                     {
-                        name: "username",
+                        name: "email",
                         type: "varchar"
-                    },
-                    {
-                        name: "chat",
-                        type: "boolean",
-                        default: true
-                    },
-                    {
-                        name: "updated_at",
-                        type: "timestamp",
-                        default: "now()"
                     },
                     {
                         name: "created_at",
                         type: "timestamp",
                         default: "now()"
-                    }
+                    },                    
                 ]
             })
         )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("settings");
+        await queryRunner.dropTable("users");
     }
 
 }
