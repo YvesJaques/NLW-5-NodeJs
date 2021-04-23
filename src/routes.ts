54533544f2078173234e25baea5aeb6b6f973bcb
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-04-23 10:57:12 -0300

    Cadastro de mensagens

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { MessagesController } from "./controllers/MessagesController";
 import { SettingsController } from "./controllers/SettingsController";
 import { UsersController } from "./controllers/UsersController";
 
@@ -7,8 +8,10 @@ const routes = Router();
 
 const settingsController = new SettingsController();
 const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 routes.post("/settings", settingsController.create)
 routes.post("/users", usersController.create)
+routes.post("/messages", messagesController.create)
 
 export { routes }
